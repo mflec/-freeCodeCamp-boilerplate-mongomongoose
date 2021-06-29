6821c29c37f7231f785b110f2ede8b00b9655679
Glitch (knowing-intriguing-tanker) <none>
2021-06-29 19:10:08 +0000

    last commit for now

--- myApp.js
+++ myApp.js
@@ -24,7 +24,7 @@ mili.save(function(err, data) {
 
 
 const arrayOfPeople= [ {name: "Juan", age: 22, favoriteFoods: ["Anything except meat"]},
-  {name: "Jere", age: 19, favoriteFoods: ["Anything"]},
+  {name: "Jere", age: 19, favoriteFoods: ["Burrito "]},
   {name: "Javier", age: 44, favoriteFoods: ["beer"]}
 ]
 
@@ -91,21 +91,69 @@ Person.findOne({name: personName}, function (err, people) { //PROMISE
 })
 };
 
-const removeById = (personId, done) => {
-  done(null /*, data*/);
+//findByIdAndRemove, findOneAndRemove son como los
+//métodos de actualización anteriores.
+//Pasan el documento eliminado a la base de datos. 
+
+const removeById = function(personId, done) {
+  Person.findByIdAndRemove(
+    personId,
+    (err, removedDoc) => {
+      if(err) return console.log(err);
+      done(null, removedDoc);
+    }
+  ); 
 };
+
+//Model.remove() es útil para eliminar 
+//tds los documentos que coinciden con los criterios dados
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, (err, removedDoc) => {
+      if(err) return console.log(err);
+      done(null, removedDoc);
+    })
 };
+
+
+/*Si no pasa la devolución de llamada como último argumento a Model.find()
+(o a los otros métodos de búsqueda), la consulta no se ejecuta. 
+Puede almacenar la consulta en una variable para su uso posterior. 
+Este tipo de objeto le permite crear una consulta utilizando sintaxis 
+de encadenamiento. La búsqueda de base de datos real se ejecuta cuando 
+finalmente encadena el método .exec(). Siempre debe pasar su devolución 
+de llamada a este último método. */
+
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+const findedPeople= Person.find({ favoriteFoods: foodToSearch });                     
+findedPeople.sort({name: 1 }); // Here: 1 for ascending	order and -1 for descending order. Array method
+findedPeople.limit(2); // return array which has 2 items in it. Array method
+findedPeople.select({ age: 0})
+// Here: 0 means false and thus hide name property; 1 means true so age property will show.
+findedPeople.exec(function(err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
+
+/*
+Segun los consejos se SUPONE que deberia funcionar también asi:
+Person.find({ favoriteFoods: foodToSearch });                     
+.sort({name: 1 }); 
+.limit(2); 
+.select({ age: 0})
+.exec(function(err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
+};
+pero eso no pasaba los test
+
+*/
+
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
